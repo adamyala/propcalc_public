ec513b024e3589438515e585b4f330ca3e07d615
adamyala <adamkyala@gmail.com>
2014-05-23 19:09:26 -0500

    dirty fucking commmas

--- PropCalc.py
+++ PropCalc.py
@@ -5,11 +5,11 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
 	# Convert latitude and longitude to 
 	# spherical coordinates in radians.
 	degrees_to_radians = math.pi/180.0
-        
+
 	# phi = 90 - latitude
 	phi1 = (90.0 - lat1)*degrees_to_radians
 	phi2 = (90.0 - lat2)*degrees_to_radians
-        
+
 	# theta = longitude
 	theta1 = long1*degrees_to_radians
 	theta2 = long2*degrees_to_radians
@@ -23,10 +23,10 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
     # distance = rho * arc length
     
 	cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + math.cos(phi1)*math.cos(phi2))
-    arc = math.acos( cos )
+	arc = math.acos(cos)
 
-    # Remember to multiply arc by the radius of the earth 
-    # in your favorite set of units to get length.
+	# Remember to multiply arc by the radius of the earth 
+	# in your favorite set of units to get length.
 	return arc * 3960
 
 def csv_to_list(file_path):
@@ -36,30 +36,46 @@ def csv_to_list(file_path):
 		data.append(row.strip().split(','))
 	return data
 
-#Account Number
-#Owner Name
-#Property Address
-#City
-#State
-#Impr Sqft 
-#Land Sqft 
-#Built
-#Note 1
-#Note 2
-#Sales Price 
-#Sale Date
+#Account Number 46
+#Owner Name 13
+#Property Address 126
+#City 127
+#State 130
+#County 128
+#Impr Sqft 7
+#Land Sqft 65
+#Built 193
+#Note 1 9
+#Note 2 144
+#Sales Price 146
+#Sale Date 145
 #$/Sqft 
 #Age
 #Distance 
 
 def sanitize_raw(source):
+	first = True
 	result = []
-	for each source_row in source:
-		result_row = [""] * 15
-		result_row[0] = source_row[46]
-		print source_row[46]
+	print source[0]
+	print source[0][126]
+	# for a in range(len(source)):
+		# if first:
+		# 	first = False
+		# else:
+		# 	print source[a][126]
+		# 	if not source[a][126].find('('):
+		# 		result_row = [''] * 16
+		# 		result_row[0] = source[a][46]
+		# 		result.append(result_row)
+	return result
 
+def main(file_path):
+	raw = csv_to_list(file_path)
+	data = sanitize_raw(raw)
 
+	#print data[0]
+
+main('/home/adam/Code/PropCalc/test.csv')
 
 
 
