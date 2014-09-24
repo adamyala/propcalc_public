6cf01ac98e2ec18ebe0b020929f8ebe2064773a4
Adam <adamkyala@gmail.com>
2014-09-24 13:41:45 -0500

    added dynamic computation to market income approach

--- static/js/propcalc.js
+++ static/js/propcalc.js
@@ -1,3 +1,12 @@
+$(document).on("keyup", function() {
+    putVal(mult(getVal('fldCompRent'),getVal('txtBldgSqft')),'txtSubt1');
+    putVal(multA(getVal('fldVaca'),getVal('txtSubt1')),'txtSubt2');
+    putVal(addr(getVal('txtSubt1'),getVal('txtSubt2')),'txtSubt3');
+    putVal(multA(getVal('fldExp'),getVal('txtSubt3')),'txtSubt4');
+    putVal(addr(getVal('txtSubt3'),getVal('txtSubt4')),'txtSubt5');
+    putVal(diviA(getVal('fldCap'),getVal('txtSubt5')),'txtSubt6');
+    putVal(divi(getVal('txtBldgSqft'),getVal('txtSubt6')),'txtSubt7');
+});
 Number.prototype.formatNumber = function(c, d, t)
 {
     var n = this, 
@@ -9,24 +18,61 @@ Number.prototype.formatNumber = function(c, d, t)
     j = (j = i.length) > 3 ? j % 3 : 0;
     return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
-function formatMoney(input)
+function toMoney(input)
 {
-    return "$ " + input
+    return "$ " + input;
 };
-function CleanNum(input1)
+function toNum(input)
 {
-    var source = input1
-    source = source.replace(/,/g,'');
-    source = source.replace(/$/g,'')
-    return source;
+    var source = input;
+    source = source.split('$').join('')
+    source = source.split(',').join('')
+    return source.trim();
 };
-function MultInputs(input1, input2, outputName)
+function getVal(input)
 {
-    var total = 1;
-    var ele1 = document.getElementsByClassName(input1);
-    var ele2 = document.getElementById(input2);
-    total *= parseFloat(CleanNum(ele1[0].value));
-    total *= parseFloat(CleanNum(ele2.innerHTML));
-    var Display = document.getElementById(outputName);
-    Display.innerHTML = (total).formatMoney(formatNumber(2,".",","));
+    if (input.substring(0,3) == 'fld') {
+        result = parseFloat(toNum(document.getElementById(input).value));
+    }
+    else {
+        result = parseFloat(toNum(document.getElementById(input).innerHTML));
+    }
+    return result;
 };
+function putVal(source, input)
+{
+    var Display = document.getElementById(input);
+    Display.innerHTML = toMoney(source.formatNumber(2,".",","));
+};
+function mult(input1, input2)
+{
+    return input1 * input2;
+};
+function multA(input1, input2)
+{
+    return -( input1 / 100 ) * input2;
+    // return input2;
+};
+function divi(input1, input2)
+{
+    return input2 / input1;
+}
+function diviA(input1, input2)
+{
+    return input2 / (input1 / 100);
+};
+function addr(input1, input2)
+{  
+    return input1 + input2;
+}
+
+// function multInputs(inputField, inputText, outputName)
+// {
+//     var total = 1;
+//     var ele1 = document.getElementById(inputField);
+//     var ele2 = document.getElementById(inputText);
+//     total *= parseFloat(CleanNum(ele1.value));
+//     total *= parseFloat(CleanNum(ele2.innerHTML));
+//     var Display = document.getElementById(outputName);
+//     Display.innerHTML = formatMoney(total.formatNumber(2,".",","));
+// };
